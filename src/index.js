96421fce062bda929bd437df574f09c7e8a1a22f
Mozzie <zhouzhijun@dongfangfuli.com>
2023-08-04 15:23:57 +0800

    learn react-dom render knowledge

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM  from 'react-dom'
 
 
+// react 自动加工DOM
+ReactDOM.render(
+    React.createElement("h2",{className:'aaaa'},"欢迎来到React世界"),
+    document.getElementById('root'))
 
-ReactDOM.render(<h1>1111111111111</h1>,document.getElementById('root'))
+
+// ReactDOM.render(<h1>1111111111111</h1>,document.getElementById('root'))
